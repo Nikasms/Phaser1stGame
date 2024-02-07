dbc8bff0d08e0efc9ca64eb0a6d5db36c87d28a4
Nikasms <148072883+Nikasms@users.noreply.github.com>
2024-02-07 13:16:07 +0200

    додали платформи, крок 3

--- game.js
+++ game.js
@@ -28,7 +28,14 @@ this.load.spritesheet('dude',
 function create ()
 {//розмір картинок
     this.add.image(400, 300, 'sky');
-    this.add.image(400, 300, 'star');
+// додали платформи
+    platforms = this.physics.add.staticGroup();
+
+    platforms.create(400, 568, 'ground').setScale(2).refreshBody();
+
+    platforms.create(600, 400, 'ground');
+    platforms.create(50, 250, 'ground');
+    platforms.create(750, 220, 'ground');
 
 }
 
