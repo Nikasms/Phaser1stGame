d4975b59e4e65d04e8341f01b8ddb84b6024d834
Nikasms <148072883+Nikasms@users.noreply.github.com>
2024-02-07 13:09:11 +0200

    Завантажили ресурси

--- game.js
+++ game.js
@@ -1,3 +1,4 @@
+//Створили конфігурацію гри
 var config = {
     type: Phaser.AUTO,
     width: 800,
@@ -12,7 +13,14 @@ var config = {
 var game = new Phaser.Game(config);
 
 function preload ()
-{
+{ this.load.image('sky', 'assets/sky.png');
+this.load.image('ground', 'assets/platform.png');
+this.load.image('star', 'assets/star.png');
+this.load.image('bomb', 'assets/bomb.png');
+this.load.spritesheet('dude', 
+    'assets/dude.png',
+    { frameWidth: 32, frameHeight: 48 }
+);
 }
 
 function create ()
